41ac29d9930bf53729ba028808d10c50bc34d6d2
Clam <dfunking@gmail.com>
2018-11-28 21:25:09 -0600

    Added clean function

--- base/Client.js
+++ base/Client.js
@@ -73,6 +73,20 @@ class Sif extends Client {
     return false;
   }
 
+  async clean(client, text) {
+    if (text && text.constructor.name == "Promise")
+      text = await text;
+    if (typeof evaled !== "string")
+      text = require("util").inspect(text, {depth: 1});
+
+    text = text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203))
+      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+
+    return text;
+  }
+
   getSettings (guild) {
     const defaults = this.config.defaultSettings || {};
     const guildData = this.settings.get(guild.id) || {};
