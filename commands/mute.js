7ed30426ff73645a38952ce21c83319c3a2f1ca5
Clam <dfunking@gmail.com>
2018-11-29 20:06:55 -0600

    Added shit

--- commands/mute.js
+++ commands/mute.js
@@ -15,11 +15,21 @@ class Mute extends Command {
 
   async run(message, args){
     let member = message.mentions.members.first();
+    
     if(!member) {
-      let collectedMessage = await this.client.awaitReply(message, "What member would you like to mute?", 30000)
+      let collectedMessage = await this.client.awaitReply(message, "What member would you like to mute?", 30000);
       member = collectedMessage.mentions.members.first();
     }
-    console.log(member);
+
+    if(!member) return message.channel.send("You must supply a member");
+    if(member.roles.highest.position >= message.member.roles.highest.position) message.channel.send("You may not do that.");
+    if(!member.kickable) return message.channel.send("I may not do that.");
+
+    //MuteRole
+    let muteRole  = message.guild.roles.find(r => r.name === message.settings.muteRole);
+    if(!muteRole) return message.channel.send("You must configure a muted role.");
+
+    console.log(muteRole);
   }
 }
 
